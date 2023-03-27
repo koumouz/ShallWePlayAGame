1df6de4f0bcfb2b6f7f37696aa5db88600bd94a3
Constantin Koumouzelis <liquidrive@users.noreply.github.com>
2023-03-27 15:53:54 -0700

    Added variables to toggle image creation and max tokens so I don't go bankcrupt

--- server.mjs
+++ server.mjs
@@ -14,15 +14,18 @@ app.use(express.static(path.join(__dirname, '.')));
 app.use(express.json({ limit: '50mb' }));
 
 
-/* Core Promts */
+/* Core Promts and Knobs*/
 let gamePrompt = "You are an interface for a text-based video game in the style of Zork, Planetfall and Wishbringer. You are responsible for creating the narrative direction of the game. Each turn, you will give me, the player, a describing the current location of the player and the effect of their actions on the world. On my turn I will tell you where I want to go and what actions I want to take. The setting is in the late 1970s. 5 highschool kids around the ages of 16 decided to camp on a remote island off the coast of their small New England town. There are rumors of weird supernatural events on this island, perhaps something to do with secret military experiments that were possible run on the island during World War 2. They say the military was researching the occult, to gain some way to defeat the Nazis. You will take this basic premise and then expand on the story, adding new scenarios and introducing new twists and turns in the narrative. I will play one of the five kids, my name will be Rupert. You will play the other 4 characters and will give them each a name and backstory.";
 gamePrompt += "The story begins as we all step foot off the rowboat we used to reach the island. It’s twilight and we are excited to begin our adventure. Begin the first turn with a short introduction of all the characters, including me and explain the setting.";
 gamePrompt += "Only I can take actions for the player, you can never act as the player character. The player can continue taking turns up to a maximum of 10 turns. After that you should respond with a message that the game is now over and then create a satisfying ending to the story for the player."
 
-let addImagePrompt = "Additionally, generate a prompt for the DALL-E API that you can use to create an image that maps to the scene. This should always be the last sentence of your response and it should beging with IMAGE_PROMPT: and then the prompt";
+let addImagePrompt = "Additionally, create a prompt for stable diffusion to create an image that maps to the scene. This should always be the last sentence of your response and it should beging with IMAGE_PROMPT: and then the prompt";
 
 let imagePrompt = "A mysterious island at twilight surrounded by fog."
-let imageStyle = "black and white, mysterious, high quality, in the style of adventure games from the 90s, pixel art"
+let imageStyle = ", black and white only, in the style of an adventure game from the 1980s, pixel art, high quality"
+
+let createImages = false;
+let numMaxTokens = 300;
 /* End Prompts */
 
 
@@ -107,7 +110,7 @@ async function generateNextTurn(history) {
     const textRequestBody = JSON.stringify({
         model: 'gpt-3.5-turbo',
         messages: history,
-        max_tokens: 350,
+        max_tokens: numMaxTokens,
         n: 1,
         stop: null,
         temperature: 0.5,
@@ -133,11 +136,12 @@ async function generateNextTurn(history) {
     let payload = {};
     payload.text = substrs[0].trim();
 
-    if(imagePrompt != null) {
+    if(createImages == true && imagePrompt != null) {
         // Now generate the image
+        imagePrompt = imagePrompt + imageStyle;
         const imageRequestBody = JSON.stringify({
             model: 'image-alpha-001',
-            prompt: imagePrompt + imageStyle,
+            prompt: imagePrompt,
             num_images: 1,
             size: '256x256',
             response_format: 'url',
@@ -165,7 +169,7 @@ async function generateNextTurn(history) {
     // DEBUG: Show the prompts and responses
     //console.log("\nResponse: " + textData.choices[0].message.content);
     //console.log("\nText Response: " + payload.text);
-    console.log("\nImage Prompt: " + imagePrompt);
+    console.log("\nImage Prompt: " + imagePrompt + imageStyle);
 
     return payload;
 }
