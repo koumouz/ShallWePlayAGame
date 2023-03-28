4dfda9bab98a90e766ba5d7e08d826bf1bff6715
Constantin Koumouzelis <liquidrive@users.noreply.github.com>
2023-03-28 14:22:15 -0700

    More port shenanigans

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '50mb' }));
 
